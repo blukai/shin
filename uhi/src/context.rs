c8299fc0cc0cf11aca66be5ccf82dad2310a29b7
blukai <20866892+blukai@users.noreply.github.com>
2025-07-17 00:46:28 +0200

    add dt into context
    this will allow to animate stuff

--- uhi/src/context.rs
+++ uhi/src/context.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use crate::{DrawBuffer, Externs, FontService, InteractionState, TextureService};
 
 pub struct Context<E: Externs> {
@@ -5,6 +7,10 @@ pub struct Context<E: Externs> {
     pub texture_service: TextureService<E>,
     pub draw_buffer: DrawBuffer<E>,
     pub interaction_state: InteractionState,
+
+    previous_frame_start: Instant,
+    current_frame_start: Instant,
+    delta_time: Duration,
 }
 
 impl<E: Externs> Default for Context<E> {
@@ -14,6 +20,10 @@ impl<E: Externs> Default for Context<E> {
             font_service: FontService::default(),
             draw_buffer: DrawBuffer::default(),
             interaction_state: InteractionState::default(),
+
+            previous_frame_start: Instant::now(),
+            current_frame_start: Instant::now(),
+            delta_time: Duration::ZERO,
         }
     }
 }
@@ -21,10 +31,18 @@ impl<E: Externs> Default for Context<E> {
 impl<E: Externs> Context<E> {
     pub fn begin_frame(&mut self) {
         self.interaction_state.begin_frame();
+
+        self.current_frame_start = Instant::now();
+        self.delta_time = self.current_frame_start - self.previous_frame_start;
+        self.previous_frame_start = self.current_frame_start;
     }
 
     pub fn end_frame(&mut self) {
         self.draw_buffer.clear();
         self.interaction_state.end_frame();
+    }
+
+    pub fn dt(&self) -> f32 {
+        self.delta_time.as_secs_f32()
     }
 }
