85f9702bd0b14e133936fda6747ad4e328e07114
blukai <20866892+blukai@users.noreply.github.com>
2025-08-17 17:46:43 +0200

    add more wayland protocols and put all of them except teh core ones begind feature flags

---
+++ wayland-protocols/readme.txt
@@ -0,0 +1,5 @@
+standard wayland protocols:
+https://gitlab.freedesktop.org/wayland/wayland-protocols
+
+wlroots (sway and many others):
+https://gitlab.freedesktop.org/wlroots/wlr-protocols
--- wayland/build.rs
+++ wayland/build.rs
@@ -3,9 +3,33 @@ use std::fs::{self, File};
 use std::io::BufWriter;
 use std::path::PathBuf;
 
+const PROTOCOLS: &[&str] = &[
+    // NOTE: always include core protocols
+    "wayland.xml",
+    #[cfg(feature = "cursor-shape-v1")]
+    "cursor-shape-v1.xml",
+    #[cfg(feature = "fractional-scale-v1")]
+    "fractional-scale-v1.xml",
+    #[cfg(feature = "linux-dmabuf-v1")]
+    "linux-dmabuf-v1.xml",
+    #[cfg(feature = "pointer-gestures-unstable-v1")]
+    "pointer-gestures-unstable-v1.xml",
+    #[cfg(feature = "tablet-v2")]
+    "tablet-v2.xml",
+    #[cfg(feature = "viewporter")]
+    "viewporter.xml",
+    #[cfg(feature = "wlr-layer-shell-unstable-v1")]
+    "wlr-layer-shell-unstable-v1.xml",
+    #[cfg(feature = "wlr-screencopy-unstable-v1")]
+    "wlr-screencopy-unstable-v1.xml",
+    #[cfg(feature = "xdg-shell")]
+    "xdg-shell.xml",
+];
+
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
+    // TODO: mac is also unix? this should yell at you on that.
     if !env::var("CARGO_CFG_UNIX").is_ok() {
         panic!("wayland is a unix-only thing, isn't it?");
     }
@@ -13,15 +37,17 @@ fn main() {
     println!("cargo:rerun-if-changed=../wayland-scanner");
     println!("cargo:rerun-if-changed=../wayland-protocols");
 
+    let protocols_dir = PathBuf::from("../wayland-protocols");
+
     let out_dir =
         PathBuf::from(&env::var("OUT_DIR").expect("OUT_DIR env var is missing or invalid"));
     let out_file =
         File::create(out_dir.join("wayland_generated.rs")).expect("could not create out file");
     let mut w = BufWriter::new(out_file);
 
-    for entry in fs::read_dir("../wayland-protocols").expect("could not read protocols dir") {
-        let entry = entry.expect("could not read protocols dir entry");
-        let input = fs::read_to_string(entry.path()).expect("could not read protocol");
+    for protocol in PROTOCOLS {
+        let path = protocols_dir.join(protocol);
+        let input = fs::read_to_string(path).expect("could not read protocol");
         let protocol =
             wayland_scanner::parse_protocol(input.as_str()).expect("could not parse protocol");
         wayland_scanner::emit_protocol(&mut w, &protocol).expect("could not generate protocol");
